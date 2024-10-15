d0e4ea4e0b5479d9104b33627891c1e7d0a65d68
ChrisRynhoud <chris.rynhoud@gmail.com>
2024-10-15 00:53:17 -0700

    littleupdates

--- ComposterProject2/composter-web/src/index.js
+++ ComposterProject2/composter-web/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/Composter/ComposterProject2">
+    <BrowserRouter basename="/Composter">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
