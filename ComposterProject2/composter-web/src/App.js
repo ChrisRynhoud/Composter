8d6fdeb71c6657de335a1e4ace35d4f446dc684f
ChrisRynhoud <chris.rynhoud@gmail.com>
2024-10-16 01:54:56 -0700

    404UpdatesStill

--- ComposterProject2/composter-web/src/App.js
+++ ComposterProject2/composter-web/src/App.js
@@ -155,43 +155,43 @@ const App = () => {
   }, [step, speed, running]);
 
   return (
-    <Router>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sanddisplay" element={
-          <div className="App"
-               onMouseDown={handleMouseDown}
-               onMouseUp={handleMouseUp}
-               onMouseMove={handleMouseMove}
-               onContextMenu={(e) => e.preventDefault()}>
-            <div className="grid-container">
-              <SandDisplay
-                title="Falling Sand"
-                numRows={numRows}
-                numCols={numCols}
-                buttonNames={buttonNames}
-                onClick={(row, col, tool) => locationClicked(row, col, tool)}
-                grid={grid}
-                setSpeed={setSpeed}
-                selectedTool={selectedTool}
-                setSelectedTool={setSelectedTool}
-              />
-            </div>
-            <button onClick={() => setRunning(!running)}>
-              {running ? "Pause" : "Run"}
-            </button>
-            {buttonNames.map((name, index) => (
-              <button key={index} onClick={() => setSelectedTool(index)}>
-                {name}
-              </button>
-            ))}
+  <Router>
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sanddisplay" element={
+        <div className="App"
+             onMouseDown={handleMouseDown}
+             onMouseUp={handleMouseUp}
+             onMouseMove={handleMouseMove}
+             onContextMenu={(e) => e.preventDefault()}>
+          <div className="grid-container">
+            <SandDisplay
+              title="Falling Sand"
+              numRows={numRows}
+              numCols={numCols}
+              buttonNames={buttonNames}
+              onClick={(row, col, tool) => locationClicked(row, col, tool)}
+              grid={grid}
+              setSpeed={setSpeed}
+              selectedTool={selectedTool}
+              setSelectedTool={setSelectedTool}
+            />
           </div>
-        } />
-      </Routes>
-    </Router>
-  );
-};
+          <button onClick={() => setRunning(!running)}>
+            {running ? "Pause" : "Run"}
+          </button>
+          {buttonNames.map((name, index) => (
+            <button key={index} onClick={() => setSelectedTool(index)}>
+              {name}
+            </button>
+          ))}
+        </div>
+      } />
+    </Routes>
+  </Router>
+);
+
 
 export default App;
