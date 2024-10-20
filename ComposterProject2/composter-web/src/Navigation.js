5d90e9f5c95f95e16416282e866073fe7b2a0f2a
ChrisRynhoud <chris.rynhoud@gmail.com>
2024-10-20 01:21:53 -0700

    NavigationUpdates

--- ComposterProject2/composter-web/src/Navigation.js
+++ ComposterProject2/composter-web/src/Navigation.js
@@ -18,13 +18,13 @@ function Navigation() {
         <ul>
           <li><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
           <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
-          <li><NavLink to="/plantdata" activeClassName="active">Plant Data</NavLink></li>
-          <li>
-            <NavLink to="/services" activeClassName="active">Services</NavLink>
-            <ul>
-              <li><NavLink to="/service1">Service 1</NavLink></li>
-              <li><NavLink to="/service2">Service 2</NavLink></li>
-              <li><NavLink to="/service3">Service 3</NavLink></li>
+          <li><NavLink to="/plantdata" activeClassName="active">Data</NavLink></li>
+          <li className="dropdown">
+            <NavLink to="/electric" activeClassName="active">How they Work</NavLink>
+            <ul className="dropdown-menu">
+              <li><NavLink to="/electric">Electric Composter</NavLink></li>
+              <li><NavLink to="/wormbin">Worm Bin</NavLink></li>
+              <li><NavLink to="/tumbler">Tumbler</NavLink></li>
             </ul>
           </li>
           <li><NavLink to="/contact" activeClassName="active">Contact</NavLink></li>
@@ -32,11 +32,11 @@ function Navigation() {
       </div>
       <div className="bottom-row">
         <ul>
-          <li><NavLink to="/option1" activeClassName="active">Option 1</NavLink></li>
-          <li><NavLink to="/option2" activeClassName="active">Option 2</NavLink></li>
-          <li><NavLink to="/option3" activeClassName="active">Option 3</NavLink></li>
-          <li><NavLink to="/option4" activeClassName="active">Option 4</NavLink></li>
-          <li><NavLink to="/option5" activeClassName="active">Option 5</NavLink></li>
+          <li><NavLink to="/showcase" activeClassName="active">Showcase</NavLink></li>
+          <li><NavLink to="/timeline" activeClassName="active">Timeline</NavLink></li>
+          <li><NavLink to="/compostle" activeClassName="active">Compostle</NavLink></li>
+          <li><NavLink to="/resources" activeClassName="active">Resources</NavLink></li>
+          <li><NavLink to="/references" activeClassName="active">References</NavLink></li>
         </ul>
       </div>
     </nav>
