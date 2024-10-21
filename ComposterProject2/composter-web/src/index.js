8a111822f4f850bfd5dc6a9ff1d0d6c897870ecf
ChrisRynhoud <chris.rynhoud@gmail.com>
2024-10-21 03:06:13 -0700

    UpdateDataButtons

--- ComposterProject2/composter-web/src/index.js
+++ ComposterProject2/composter-web/src/index.js
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <ScrollToTop />  // Use ScrollToTop
+      <ScrollToTop />  
       <App />
     </HashRouter>
   </React.StrictMode>
