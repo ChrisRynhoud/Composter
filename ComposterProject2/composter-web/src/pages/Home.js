9e3e1bee8cf2ee92378f136e76c18f7adf758700
ChrisRynhoud <chris.rynhoud@gmail.com>
2024-10-23 08:29:31 -0700

    FinalGraphFix

--- ComposterProject2/composter-web/src/pages/Home.js
+++ ComposterProject2/composter-web/src/pages/Home.js
@@ -13,7 +13,7 @@ function Home() {
   useEffect(() => {
     axios.get('https://new-backend-app-35dbde982dde.herokuapp.com/food-scrap-data')
       .then(response => {
-        const total = response.data.reduce((acc, curr) => acc + (curr.data && curr.data.foodScrapSaved ? Number(curr.data.foodScrapSaved) : 0), 0); // Ensuring foodScrapSaved is defined
+        const total = response.data.reduce((acc, curr) => acc + (curr.foodScrapSaved ? Number(curr.foodScrapSaved) : 0), 0); // Direct access like GraphsSection.js
         console.log('Total Food Scraps:', total);
         setTotalFoodScraps(total);
       })
