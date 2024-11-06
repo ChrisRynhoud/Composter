9a5038cca8df707f3424c5f9c444555200353d03
ChrisRynhoud <chris.rynhoud@gmail.com>
2024-11-06 00:06:11 -0800

    ConciseAboutPageUpdatedShowcase

--- ComposterProject2/composter-web/src/pages/Showcase.js
+++ ComposterProject2/composter-web/src/pages/Showcase.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Showcase.css';
 
 // Importing placeholders
-import placeholder1 from '../assets/images/placeholder.jpg';
-import placeholder2 from '../assets/images/placeholder.jpg';
-import placeholder3 from '../assets/images/placeholder.jpg';
-import placeholder4 from '../assets/images/placeholder.jpg';
-import placeholder5 from '../assets/images/placeholder.jpg';
+import placeholder1 from '../assets/images/setUpGardening.jpg';
+import placeholder2 from '../assets/images/AllComposters.jpg';
+import placeholder3 from '../assets/images/wormsarrived.jpg';
+import placeholder4 from '../assets/images/wormmixing.jpg';
+import placeholder5 from '../assets/images/comingtogether.jpg';
 import placeholder6 from '../assets/images/placeholder.jpg';
 import placeholder7 from '../assets/images/placeholder.jpg';
 import placeholder8 from '../assets/images/placeholder.jpg';
@@ -33,12 +34,14 @@ import placeholder29 from '../assets/images/placeholder.jpg';
 import placeholder30 from '../assets/images/placeholder.jpg';
 
 const Showcase = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalImage, setModalImage] = useState('');
   const gallery = [
-    { src: placeholder1, name: 'Photo 1', date: '01/01/2021', tidbit: 'A beautiful sunrise.' },
-    { src: placeholder2, name: 'Photo 2', date: '02/01/2021', tidbit: 'A bustling cityscape.' },
-    { src: placeholder3, name: 'Photo 3', date: '03/01/2021', tidbit: 'A tranquil forest.' },
-    { src: placeholder4, name: 'Photo 4', date: '04/01/2021', tidbit: 'A serene beach.' },
-    { src: placeholder5, name: 'Photo 5', date: '05/01/2021', tidbit: 'A snowy mountain.' },
+    { src: placeholder1, name: 'Gardening Setup!', date: '10/30/2024', tidbit: 'The students set up the gardening area!' },
+    { src: placeholder2, name: 'Our Composters Arrived!', date: '10/30/2024', tidbit: 'All of our composters have finally arrived!' },
+    { src: placeholder3, name: 'Ooh Worms!', date: '11/5/2024', tidbit: 'A bunch of our worms showed up!' },
+    { src: placeholder4, name: 'Stir it Up!', date: '11/5/2024', tidbit: 'Ms. Elyse is showing us how to stir up some worm paydirt!' },
+    { src: placeholder5, name: 'Things Are Really Coming Together!', date: '11/5/2024', tidbit: 'Wow, everything is really shaping up here!' },
     { src: placeholder6, name: 'Photo 6', date: '06/01/2021', tidbit: 'A vibrant sunset.' },
     { src: placeholder7, name: 'Photo 7', date: '07/01/2021', tidbit: 'A colorful market.' },
     { src: placeholder8, name: 'Photo 8', date: '08/01/2021', tidbit: 'A peaceful lake.' },
@@ -66,20 +69,40 @@ const Showcase = () => {
     { src: placeholder30, name: 'Photo 30', date: '30/01/2021', tidbit: 'A peaceful meadow.' },
   ];
 
+  const handleClick = (src) => { 
+    setModalImage(src); 
+    setModalVisible(true); 
+  };
+
+  const closeModal = () => { 
+    setModalVisible(false); 
+  };
+
+  
   return (
-    <div className="gallery">
-      {gallery.map((item, index) => (
-        <div key={index} className="photo">
-          <img src={item.src} alt={item.name} />
-          <div className="info">
-            <h2>{item.name}</h2>
-            <p>{item.date}</p> 
-            <p>{item.tidbit}</p>
+    <div>
+      <div className="advanced-gallery">
+        {gallery.map((item, index) => (
+          <div key={index} className="advanced-photo" onClick={() => handleClick(item.src)}>
+            <img src={item.src} alt={item.name} />
+            <div className="advanced-info">
+              <h2>{item.name}</h2>
+              <p>{item.date}</p> 
+              <p>{item.tidbit}</p>
+            </div>
           </div>
+        ))}
+      </div>
+      
+      {modalVisible && (
+        <div className="modal" onClick={closeModal}>
+          <span className="close" onClick={closeModal}>&times;</span>
+          <img className="modal-content" src={modalImage} alt="" />
         </div>
-      ))}
+      )}
     </div>
   );
 };
 
 export default Showcase;
+
